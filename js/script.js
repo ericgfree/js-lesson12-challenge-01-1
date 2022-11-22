c2c88cf9892f97187fccc18f95b0543b84decb81
ericgfree <ericgfreeman@gmail.com>
2022-11-22 13:26:12 -0500

    Added image display and button function

--- js/script.js
+++ js/script.js
@@ -2,11 +2,30 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
 const getImage = async function () {
-    const res = await fetch (
+    const res = await fetch(
         "https://picsum.photos/v2/list?limit=100"
 );
         const images = await res.json();
-        console.log(images);
+        // console.log(images);
+        selectRanomImage(images);
 }
-getImage();
+const selectRanomImage = function (images) {
+        const radomIndex = Math.floor(Math.random() * images.length);
+        const radomImage = images[radomIndex];
+        // console.log(radomIndex);
+        // console.log(radomImage);
+        displayImage(radomImage);
+};
+
+const displayImage = function (radomImage) {
+        const author = radomImage.author;
+        const imageAddress = radomImage.download_url;
+        authorSpan.innerText = author;
+        img.src = imageAddress;
+        imgDiv.classList.remove("hide");
+}
+button.addEventListener("click", function () {
+        getImage();
+});
